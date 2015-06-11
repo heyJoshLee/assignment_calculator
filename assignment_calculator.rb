53a7903429208a3fcdd61fe7f4089afda89fe6af
Josh Lee <Lee45276@gmail.com>
2015-06-11 22:02:18 +0800

    initial commit

---
+++ assignment_calculator.rb
@@ -0,0 +1,26 @@
+# Code up your own calculator from the lecture. Make sure you can 
+#   run it from the command line. Save the calculator file in a 
+#   directory, and initialize the directory as a git repository. 
+#   Make sure this isn't nested in another existing git repository. 
+#   Then, push this git repository to Github.
+
+# Hint: review this section on “do/while” loops on how to keep asking 
+#   for input until some condition is met: 
+#   http://www.gotealeaf.com/books/ruby/read/loops_iterators#dowhileloops
+
+def say(msg)
+  puts "=> #{msg}"
+end
+
+puts "Welcome to my calculator." 
+
+say("Please enter your first number.")
+num1 = gets.chomp
+
+say("Please enter your second number.")
+num2 = gets.chomp
+
+
+
+
+
